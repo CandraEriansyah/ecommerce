cb67bdf040020e0e1d3242713ad992e7f9f45144
CandraEriansyah <candraeriansyah@gmail.com>
2024-05-13 22:25:47 +0700

    membuat web ecommerce reactJs

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Footer } from "./Footer";
+export { default as Main } from "./main";
+export { default as Navbar } from "./Navbar";
+export { default as Product } from "./Products";
